e1ea14a9c99d744e50439770103be8a53ad5ee83
vasiekig-droid <vasiekig@gmail.com>
2026-06-05 22:29:08 +0200

    Add files via upload

---
+++ neurocourse_tracker_v31/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "neurocourse-tracker-v31";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./data.js"];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
