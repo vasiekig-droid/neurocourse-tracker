d17b206a43b1a474e0c15178675c36cd7d96b048
vasiekig-droid <vasiekig@gmail.com>
2026-06-06 16:57:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "neurocourse-tracker-v39";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const CACHE_NAME = "neurocourse-tracker-v41";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
-self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); if (self.clients && self.clients.claim) self.clients.claim(); });
-self.addEventListener("fetch", e => { e.respondWith(fetch(e.request).then(resp => { const copy = resp.clone(); caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy)).catch(()=>{}); return resp; }).catch(() => caches.match(e.request))); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener("fetch", e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
